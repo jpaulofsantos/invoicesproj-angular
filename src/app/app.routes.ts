0971b03a2aaf231a1f75c614c6dcf5cbed51c328
JoaoPaulo <jpaulofsantos@gmail.com>
2024-04-23 20:22:43 -0300

    login html, first user interface and service

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './component/login/login.component';
+import { RegisterComponent } from './component/register/register.component';
+import { ResetpasswordComponent } from './component/resetpassword/resetpassword.component';
+import { VerifyComponent } from './component/verify/verify.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent},
+  { path: 'resetpassword', component: ResetpasswordComponent},
+  { path: 'user/verify/account/:key', component: VerifyComponent},
+  { path: 'user/verify/password/:key', component: VerifyComponent},
+  { path: '**', component: LoginComponent},
+];
